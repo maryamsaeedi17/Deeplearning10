131891fd1890c694406aa3767cf4c61b06a7fe5b
maryamsaeedi17 <m.saeedi64@gmail.com>
2024-01-15 01:10:09 +0330

    face_bank_and_identification

---
+++ create_face_bank.py
@@ -0,0 +1,35 @@
+import os
+import cv2
+import numpy as np
+from insightface.app import FaceAnalysis
+
+app = FaceAnalysis(providers=["CPUExecutionProvider"], name="buffalo_s")
+app.prepare(ctx_id=0, det_size=(640, 640))
+
+face_bank_path = "./face_bank/"
+
+face_bank = []
+
+for person_name in os.listdir(face_bank_path):
+    file_path = os.path.join(face_bank_path, person_name)
+    if os.path.isdir(file_path):
+        for image_name in os.listdir(file_path):
+            if image_name != ".DS_Store":
+                image_path = os.path.join(file_path, image_name)
+                print(image_path)
+                image = cv2.imread(image_path)
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                result = app.get(image)
+
+                if len(result)>1:
+                    print("Warning")
+                    continue
+
+                # print(len(result))
+
+                embedding = result[0]["embedding"]
+                my_dict = {"name": person_name, "embedding": embedding}
+                face_bank.append(my_dict)
+
+
+np.save("face_bank.npy", face_bank)
---
+++ face_identification.py
@@ -0,0 +1,46 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+from insightface.app import FaceAnalysis
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--image", type=str, help="Path of the inferential image")
+
+arg = parser.parse_args()
+
+img_path = arg.image
+
+face_bank = np.load("face_bank.npy", allow_pickle=True )
+
+app = FaceAnalysis(providers=["CPUExecutionProvider"], name="buffalo_s")
+app.prepare(ctx_id=0, det_size=(640, 640))
+
+#input = cv2.imread(img_path)
+input = cv2.imread("Inputs/a1.jpg")
+input = cv2.cvtColor(input, cv2.COLOR_BGR2RGB)
+
+results = app.get(input)
+print(len(results))
+
+threshold = 25
+
+for result in results:
+    cv2.rectangle(input, (int(result.bbox[0]), int(result.bbox[1])), (int(result.bbox[2]), int(result.bbox[3])), (0, 255, 0), 2)
+
+    for person in face_bank:
+        face_bank_person_embedding = person["embedding"]
+        new_person_embedding = result["embedding"]
+
+        dist = np.sqrt(np.sum((face_bank_person_embedding - new_person_embedding)**2))
+        
+        if dist<threshold:
+            cv2.putText(input, person["name"], (int(result.bbox[0]) - 10, int(result.bbox[1] - 10)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+            break
+
+    else:
+        cv2.putText(input, "Unknown", (int(result.bbox[0]) - 10, int(result.bbox[1] - 10)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+plt.imshow(input)
+plt.show()
