4ef2256b02757b9b11f03d9679b4fa3efcdf5795
maryamsaeedi17 <m.saeedi64@gmail.com>
2024-01-16 00:44:39 +0330

    update

--- face_identification.py
+++ face_identification.py
@@ -18,7 +18,7 @@ app = FaceAnalysis(providers=["CPUExecutionProvider"], name="buffalo_s")
 app.prepare(ctx_id=0, det_size=(640, 640))
 
 #input = cv2.imread(img_path)
-input = cv2.imread("Inputs/a1.jpg")
+input = cv2.imread("Inputs/a2.jpg")
 input = cv2.cvtColor(input, cv2.COLOR_BGR2RGB)
 
 results = app.get(input)
@@ -36,11 +36,11 @@ for result in results:
         dist = np.sqrt(np.sum((face_bank_person_embedding - new_person_embedding)**2))
         
         if dist<threshold:
-            cv2.putText(input, person["name"], (int(result.bbox[0]) - 10, int(result.bbox[1] - 10)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+            cv2.putText(input, person["name"], (int(result.bbox[0]), int(result.bbox[1] - 2)), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1, cv2.LINE_AA)
             break
 
     else:
-        cv2.putText(input, "Unknown", (int(result.bbox[0]) - 10, int(result.bbox[1] - 10)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.putText(input, "Unknown", (int(result.bbox[0]), int(result.bbox[1] - 2)), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 0, 0), 1, cv2.LINE_AA)
 
 plt.imshow(input)
 plt.show()
